d0bf98cc0ea0bd12b6a24c6d4173c3b1a5b2e8a5
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-07 15:58:07 -0300

    3 new routes,to process a user, another to find user by cpf and mock to verify status from cpf.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('api/v1/users')->group(function () {
+    Route::get('/{cpf}', [UserController::class, 'show']);
+    Route::post('/process', [UserController::class, 'process']);
 });
+
+// Mock API, to return a random status from cpf statuses //
+Route::get('/cpf/status/{cpf}', function (string $cpf) {
+    $statuses = ['clean', 'pending', 'negative'];
+    return response()->json([
+        'status' => $statuses[array_rand($statuses)]
+    ]);
+})->where('cpf', '[0-9]{11}');
