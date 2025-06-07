841641d43cf069f77a82bec5b37df0ac3a03b2ab
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-07 15:54:37 -0300

    CPF service integration

---
+++ app/Integrations/CpfIntegration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Integrations;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class CpfIntegration
+{
+    protected string $LOG_CONTEXT = '[CPF INTEGRATION]';
+
+    /**
+     * @param string $cpf
+     * @return array|null
+     * @throws Throwable
+     */
+    public function checkCpf(string $cpf): ?array
+    {
+        Log::info("$this->LOG_CONTEXT - Starting call to cpf", ['$cpf' => $cpf]);
+
+        return retry(3, function () use ($cpf) {
+            $response = Http::get("http://localhost:8001/api/cpf/status", ['cpf' => $cpf]);
+
+            if ($response->failed()) {
+                Log::error("$this->LOG_CONTEXT - error call to cpf", ['status' => $response->status()]);
+                throw new \Exception("Error validating CPF");
+            }
+
+            Log::info("$this->LOG_CONTEXT - completed successfully");
+            
+            return $response->json();
+        }, 1000);
+    }
+}
