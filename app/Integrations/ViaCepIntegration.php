42667192d5e3f0dc72ec3b3b79764435806d3fc8
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-07 15:54:44 -0300

    CEP service integration

---
+++ app/Integrations/ViaCepIntegration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Integrations;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class ViaCepIntegration
+{
+    protected string $LOG_CONTEXT = '[VIA CEP INTEGRATION]';
+
+    /**
+     * @param string $cep
+     * @return array|null
+     * @throws Throwable
+     */
+    public function checkAddress(string $cep): ?array
+    {
+        Log::info("$this->LOG_CONTEXT - Starting call to ViaCep", ['cep' => $cep]);
+
+        return retry(3, function () use ($cep) {
+            $response = Http::get("https://viacep.com.br/ws/{$cep}/json/");
+
+            if ($response->failed()) {
+                Log::error("$this->LOG_CONTEXT -  call failure", ['cep' => $cep, 'status' => $response->status()]);
+                throw new \Exception('Error searching for zip code');
+            }
+
+            Log::info("$this->LOG_CONTEXT - ViaCep call completed successfully");
+
+            return $response->json();
+        }, 1000);
+    }
+}
