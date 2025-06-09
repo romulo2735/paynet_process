9eb089ed0f62a8551248c1e8424edc1931c1eb38
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-09 17:04:59 -0300

    enable routes api.php

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+use OpenApi\Annotations as OA;
+
+
+Route::prefix('/v1/users')->group(function () {
+    Route::get('/{cpf}', [UserController::class, 'show']);
+    Route::post('/process', [UserController::class, 'process']);
+});
+
+// Mock API, to return a random status from cpf statuses //
+Route::get('/cpf/status/{cpf}', function (string $cpf) {
+    $statuses = ['clean', 'pending', 'negative'];
+    return response()->json([
+        'status' => $statuses[array_rand($statuses)]
+    ]);
+})->where('cpf', '[0-9]{11}');
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('api/v1/users')->group(function () {
-    Route::get('/{cpf}', [UserController::class, 'show']);
-    Route::post('/process', [UserController::class, 'process']);
+Route::get('/', function () {
+    return view('welcome');
 });
-
-// Mock API, to return a random status from cpf statuses //
-Route::get('/cpf/status/{cpf}', function (string $cpf) {
-    $statuses = ['clean', 'pending', 'negative'];
-    return response()->json([
-        'status' => $statuses[array_rand($statuses)]
-    ]);
-})->where('cpf', '[0-9]{11}');
