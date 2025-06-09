1f55cdf4dae1ec8ea1b07fc414bce19c09c25743
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-09 19:21:24 -0300

    remove fields unnecessary

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('cpf');
             $table->string('cep');
             $table->json('address')->nullable();
@@ -21,7 +20,6 @@ return new class extends Migration
             $table->json('cpf_status')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
