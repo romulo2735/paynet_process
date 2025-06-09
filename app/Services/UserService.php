5042fdf95e57d6424f87700b7e7efecd06f7af28
Romulo Sousa <romulo.sousa@artit.com.br>
2025-06-09 19:37:44 -0300

    chore: insert new logs

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\DTOs\UserProcessDTO;
 use App\Jobs\ProcessUserJob;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
 class UserService
@@ -12,10 +13,14 @@ class UserService
     public function handle(UserProcessDTO $dto): void
     {
         $cacheKey = "process-user-{$dto->cpf}";
+        Log::info("Verifying if CPF is in cache: $cacheKey");
+
         if (!Redis::exists($cacheKey)) {
             Redis::setex($cacheKey, 300, json_encode($dto));
 
             ProcessUserJob::dispatch($dto);
         }
+
+        Log::info("CPF in cache:");
     }
 }
